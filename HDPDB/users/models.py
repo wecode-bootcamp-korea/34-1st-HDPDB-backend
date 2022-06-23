f1477a08d92ef3802f933b9430f56726576b15c9
koceanie <kocean@xanny.us>
2022-06-23 14:12:22 +0900

    FIXED : separate product/models.py/product database, re-migrate

--- HDPDB/users/models.py
+++ HDPDB/users/models.py
@@ -1,11 +1,10 @@
 from django.db import models
+from core.utils import TimestampZone
 
-class User(models.Model):
+class User(TimestampZone):
     name = models.CharField(max_length=45)
     email = models.EmailField(max_length=45, unique=True)
     password = models.CharField(max_length=200)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         db_table = 'users'
